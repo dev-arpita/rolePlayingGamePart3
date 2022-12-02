5fa3a159e5f6175723db8deed78492c7a625cfd9
Arpita Roy <arpita.roy.workar@gmail.com>
2022-12-02 20:15:15 +0530

    Launch the first attack

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ import characterData from './data.js'
 import Character from './Character.js'
 
 /*
-CHALLENGE:
-1 Set up a function called attack()
-2 Wire up the attack button so when pressed, the 
-function logs out 'attack button working!'
-3 Do this without declaring any new variables
+CHALLENGE
+1. Make the attack button trigger a dice roll for both
+wizard and orc (which method on the constructor does that??)
+2. Make sure the new dice score shows in the app
 **hint.md for help!!**
 */
 
 function attack(){
-    console.log('attack button working!')
+    render()
 }
 
 function render() {
     document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
     document.getElementById('monster').innerHTML = orc.getCharacterHtml();
 }
-document.getElementById('attack-button').addEventListener('click', attack)
+
+document.getElementById("attack-button").addEventListener('click', attack)
+
 const wizard = new Character(characterData.hero)
 const orc = new Character(characterData.monster)
 render()
