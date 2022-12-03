5085a6c436c0cda2985095d997803048aa90c823
Arpita Roy <arpita.roy.workar@gmail.com>
2022-12-03 19:24:42 +0530

    Fix the dice render problem1

--- Character.js
+++ Character.js
@@ -1,15 +1,10 @@
 import {getDiceRollArray, getDicePlaceholderHtml} from './utils.js'
-
 /*
-CHALLENGE
-1. In the Character constructor, create a new
-property called "diceArray".
-2. Set diceArray equal to whatever is returned
-by the getDicePlaceholderHtml function (think
-what two things you need to do to make this work!!).
-3. Instead of rendering diceHtml in the getCharacterHtml
-method, render diceArray.
-4. Delete any unnecessary code.
+Challenge
+1. Add a new property called currentDiceScore to each character's
+ data and initialise it to an empty array.
+2. Rewrite the getDiceHtml method so it updates currentDiceScore
+ with the values returned by getDiceRollArray.
 */
 
 function Character(data) {
@@ -18,21 +13,19 @@ function Character(data) {
     this.diceArray = getDicePlaceholderHtml(this.diceCount)
 
     this.getDiceHtml = function(diceCount) {
-        return getDiceRollArray(diceCount).map(function(num){
-            return  `<div class="dice">${num}</div>`
-        }).join('')
+        this.currentDiceScore = getDiceRollArray(this.diceCount)
     }
 
     this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount, diceArray } = this;
-        let diceHtml = this.getDiceHtml(diceCount);
+        const { elementId, name, avatar, health, diceCount } = this;
+
            return `
             <div class="character-card">
                 <h4 class="name"> ${name} </h4>
                 <img class="avatar" src="${avatar}" />
                 <div class="health">health: <b> ${health} </b></div>
                 <div class="dice-container">
-                    ${diceArray}
+                    ${this.diceArray}
                 </div>
             </div>`
     }
