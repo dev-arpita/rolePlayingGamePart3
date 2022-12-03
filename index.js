d02672fe8f57f29b1b2dd77dc950f533347835dd
Arpita Roy <arpita.roy.workar@gmail.com>
2022-12-03 10:12:35 +0530

    Dice placeholder

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 import characterData from './data.js'
 import Character from './Character.js'
 
-/*
-CHALLENGE
-1. Make the attack button trigger a dice roll for both
-wizard and orc (which method on the constructor does that??)
-2. Make sure the new dice score shows in the app
-**hint.md for help!!**
-*/
-
 function attack(){
     render()
 }
-
 function render() {
     document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
     document.getElementById('monster').innerHTML = orc.getCharacterHtml();
 }
-
 document.getElementById("attack-button").addEventListener('click', attack)
-
 const wizard = new Character(characterData.hero)
 const orc = new Character(characterData.monster)
 render()
