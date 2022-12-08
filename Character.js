59bcb155d3c8e64cdb5b290b92f69feaae05e323
Arpita Roy <arpita.roy.workar@gmail.com>
2022-12-08 19:05:13 +0530

    Adding more monsters 1

--- Character.js
+++ Character.js
@@ -1,51 +1,59 @@
-import { getDiceRollArray, getDicePlaceholderHtml, getPercentage } from './utils.js'
+import characterData from './data.js'
+import Character from './Character.js'
 
-function Character(data) {
-    Object.assign(this, data)
-    this.maxHealth = this.health
+let monstersArray = ["orc", "demon", "goblin"];
 
-    this.diceArray = getDicePlaceholderHtml(this.diceCount)
+/*
+Challenge
+1. Create a function called getNewMonster.
+2. Write logic inside the function that takes the first
+monster from monstersArray and extracts that monster's
+data from characterData.
+3. Save that data to a new const called nextMonsterData.
+**hint.md for help!!**
+*/
 
-    this.getDiceHtml = function () {
-        this.currentDiceScore = getDiceRollArray(this.diceCount);
-        this.diceArray = this.currentDiceScore.map((num) =>
-            `<div class="dice">${num}</div>`).join("")
-    }
+function getNewMonster(){
+   const nextMonsterData = characterData[monstersArray.shift()]
+    console.log(nextMonsterData)
+}
+getNewMonster()
 
-    this.takeDamage = function (attackScoreArray) {
-        const totalAttackScore = attackScoreArray.reduce((total, num) => total + num)
-        this.health -= totalAttackScore
-        if (this.health <= 0) {
-            this.dead = true
-            this.health = 0
+function attack() {
+    wizard.getDiceHtml()
+    orc.getDiceHtml()
+    wizard.takeDamage(orc.currentDiceScore)
+    orc.takeDamage(wizard.currentDiceScore)
+    render()
+
+        if(wizard.dead || orc.dead){
+            endGame()
         }
-    }
-
-
-    this.getHealthBarHtml = function () {
-        const percent = getPercentage(this.health, this.maxHealth);
-        return `<div class="health-bar-outer">
-                    <div class="health-bar-inner ${percent < 26 ? "danger" : ""}"
-                            style="width:${percent}%;">
-                    </div>
-                </div>`
-    }
-
-
-    this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount } = this;
-        const healthBar = this.getHealthBarHtml();
-        return `
-            <div class="character-card">
-                <h4 class="name"> ${name} </h4>
-                <img class="avatar" src="${avatar}" />
-                <div class="health">health: <b> ${health} </b></div>
-                ${healthBar}
-                <div class="dice-container">
-                    ${this.diceArray}
-                </div>
-            </div>`
-    }
 }
 
-export default Character
+function endGame() {
+    const endMessage = wizard.health === 0 && orc.health === 0 ?
+        "No victors - all creatures are dead" :
+        wizard.health > 0 ? "The Wizard Wins" :
+            "The Orc is Victorious"
+
+    const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
+    document.body.innerHTML = `
+        <div class="end-game">
+            <h2>Game Over</h2>
+            <h3>${endMessage}</h3>
+            <p class="end-emoji">${endEmoji}</p>
+        </div>
+        `
+}
+
+document.getElementById("attack-button").addEventListener('click', attack)
+
+function render() {
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
+    document.getElementById('monster').innerHTML = orc.getCharacterHtml()
+}
+
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.monster)
+render()
